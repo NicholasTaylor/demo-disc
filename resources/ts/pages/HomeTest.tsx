ba581c739ae2994e2adc4938b4fcded0d549a786
Nicholas Taylor <t@Ylornico.la>
2023-03-26 02:03:07 -0400

    Build demo page

--- resources/ts/pages/HomeTest.tsx
+++ resources/ts/pages/HomeTest.tsx
@@ -1,16 +1,22 @@
 /** @jsx jsx */
 import React from 'react';
 import { css, jsx } from '@emotion/react';
+import CustomFonts from "../components/Fonts";
+import { fontFamily, gibson, fontSize, fontWeight } from '../constants/style';
 
 export default function HomeTest(){
     return(
-        <div
-            css={css`
-                background-color: black;
-                color:white;
-            `}
-        >
-            Hello World!
+        <div>
+            <CustomFonts />
+            <div
+                css={css`
+                    background-color: black;
+                    color:white;
+                    font-family: ${gibson}, ${fontFamily}
+                `}
+            >
+                Hello World!
+            </div>
         </div>
     )
 }
